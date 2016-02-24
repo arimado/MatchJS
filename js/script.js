493eb9127624938b5f81130c1ea46b785c1ed265
Jeremy Arimado <jarimado@gmail.com>
2016-02-24 21:54:50 +1100

    Base architecture with comments

--- js/script.js
+++ js/script.js
@@ -6,9 +6,31 @@
 	MATCH.RENDER = {}; 
 	MATCH.ACTION = {}; 
 
-	MATCH.STATE.height = 0; 
-	MATCH.STATE.width = 0; 
+	MATCH.STATE.board = []; 	// variables up in object scope so they can be shared by function down in scope
 
-	MATCH.STATE.board = []; 
+	MATCH.STATE.populateBoard = function () {
+		
+		// init 2d array with objects 
+		// randomly create pairs in array 
+
+	}
+
+	MATCH.RENDER.drawBoard = function () {
+		// Get the HTML elements in there 
+	}
+
+	MATCH.RENDER.paintBoard = function () {
+		// get the HTML data in there 
+	}
+
+	MATCH.ACTION.cardClicks = function () {
+		// state changes
+		// compare active pair functions 
+		// repaint ? 
+	}
+
+	MATCH.ACTION.init = function () {
+		// init your shit mate
+	}
 
 })(); 
