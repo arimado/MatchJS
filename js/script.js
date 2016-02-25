bce941c29be315f25df8ec2f1dbf9074bb0a5b90
Jeremy Arimado <jarimado@gmail.com>
2016-02-25 20:48:04 +1100

    working on STATE.populatePairs()

--- js/script.js
+++ js/script.js
@@ -10,23 +10,50 @@
 	MATCH.STATE.rows = 4; 
 
 	MATCH.STATE.board = []; 	// variables up in object scope so they can be shared by function down in scope
+	
+	MATCH.STATE.cards = function () {
+		// store cards here
+	}
 
 	MATCH.STATE.populateBoard = function (board, columns, rows) {
-
 		// init 2d array with objects 
-		// randomly create pairs in array 
-
+		var cardObject; 
 		for (row = 0; row < rows; row += 1) {
 			board[row] = []; 
 			for (column = 0; column < columns; column += 1) {
-				board[row].push([]);
+				cardObject = {
+					active: false, 
+					content: "string of content",
+					pairGroup: 'position: ' + row + ' - ' + column
+				}; 
+				board[row].push(cardObject);
 			}
 		}
+		return board; 
+	};
+
+	MATCH.STATE.populatePairs = function (board) {
+
+		// randomly create pairs in array 
+
+		// 2d array length
+
+		// half the number
+
+		// put double of every number in the board 
+
+			// randomise using a UTIL function first 
+
+				//if empty pairGroup place property if not continue looking for empty pairGroup 
+
+				// keep going until there is none 
+
+		var totalPairs = board.length * board[0].length / 2; 
+
+		console.log(totalPairs); 
 
 		return board; 
-
-
-	};
+	}
 
 	MATCH.RENDER.drawBoard = function () {
 		// Get the HTML elements in there 
@@ -45,7 +72,7 @@
 	MATCH.ACTION.init = function () {
 		// init your shit mate 
 
-		console.log(MATCH.STATE.populateBoard(MATCH.STATE.board, MATCH.STATE.columns, MATCH.STATE.rows)); 
+		MATCH.STATE.populatePairs(MATCH.STATE.populateBoard(MATCH.STATE.board, MATCH.STATE.columns, MATCH.STATE.rows)); 
 
 	};
 
