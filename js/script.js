c1af8dd033a083075beaec3c2243f1bf72e235b5
Jeremy Arimado <jarimado@gmail.com>
2016-02-27 21:01:18 +1100

    MATCH.RENDER.init(); - Displaying data on the DOM

--- js/script.js
+++ js/script.js
@@ -50,7 +50,6 @@
 	}; 
 
 	MATCH.STATE.populatePairs = function (populatedBoard) {
-
 		console.log('MATCH.STATE.populatePairs -------- ');
 
 		var totalObjects = populatedBoard.length; 
@@ -59,22 +58,43 @@
 
 		for (i = 0; i < totalObjects / 2; i += 1) positions.push(i);
 		for (i = 0; i < totalObjects / 2; i += 1) positions.push(i); 
-
-		console.log(positions);
 		
 		totalPositions = positions.length; 
 
 		for (i = 0; i < totalObjects; i += 1) {
-
 			var randNumPositionArray = MATCH.UTIL.random(0, positions.length - 1); 
-			var randPosition = positions[randNumPositionArray]; 
-
+			var randPosition = positions[randNumPositionArray];  
 			populatedBoard[i].pairGroup = randPosition; 
 			positions.splice(randNumPositionArray, 1); 
 		} 
-		
+
 		return populatedBoard; 
-	};
+	}; 
+
+	MATCH.RENDER.init = function (matchedBoard) { 
+
+		var wrapDiv = document.getElementById('wrap');
+		var gameElement = document.createElement('div'); 
+
+		wrapDiv.appendChild(gameElement); 
+
+		matchedBoard.forEach( function (card) {
+			var currentCardElement = document.createElement('div');
+			var currentCardPairGroup = document.createElement('p'); 
+			var currentCardState = document.createElement('p'); 
+			var currentCardContent = document.createElement('p'); 
+
+			currentCardPairGroup.innerHTML = card.pairGroup; 
+			currentCardState.innerHTML = card.active; 
+			currentCardContent.innerHTML = card.content; 
+
+			gameElement.appendChild(currentCardElement); 
+
+			currentCardElement.appendChild(currentCardPairGroup);
+			currentCardElement.appendChild(currentCardState);  
+			currentCardElement.appendChild(currentCardContent);  
+		}); 
+	}
 
 	MATCH.RENDER.drawBoard = function () {
 		// Get the HTML elements in there 
@@ -99,9 +119,7 @@
 		var populatedBoard = MATCH.STATE.populateBoard(board, col, row);
 		var matchedBoard = MATCH.STATE.populatePairs(populatedBoard);
 
-		console.log('MATCH.ACTION.init -------- ');
-		console.dir(matchedBoard); 
-
+		MATCH.RENDER.init(matchedBoard);
 	};
 
 	MATCH.ACTION.init(); 
